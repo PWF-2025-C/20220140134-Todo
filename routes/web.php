b87ca3cb5e618006a64168ed1ffbb1ec0f3bf814
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-05-24 15:29:10 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,27 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
+// Halaman awal
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Halaman dashboard (harus login & verifikasi email)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Grup route untuk user yang sudah login
 Route::middleware('auth')->group(function () {
     // Todo routes
     Route::resource('todo', TodoController::class)->except(['show']);
-    
     Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
     Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
     Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+});
 
+// Grup route untuk user yang sudah login dan terverifikasi email-nya
+Route::middleware(['auth', 'verified'])->group(function () {
     // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -31,12 +36,14 @@ Route::middleware('auth')->group(function () {
     Route::resource('categories', CategoryController::class);
 });
 
+// Grup route untuk admin
 Route::middleware(['auth', 'admin'])->group(function () {
-    // User  routes
+    // User management routes
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
     Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
     Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
 });
 
+// Routing bawaan Laravel Breeze atau Jetstream
 require __DIR__.'/auth.php';
---
+++ tugas10.md
@@ -0,0 +1,13 @@
+# Tugas 10
+
+## 1. Browser Register
+![alt text](screenshoot/tugas10/browser_register.png)
+
+## 2. Verify Email
+![alt text](screenshoot/tugas10/browser_verifyEmail.png)
+
+## 3. Mailtrap
+![alt text](screenshoot/tugas10/browser_mailtrap.png)
+
+## 4. Dashboard
+![alt text](screenshoot/tugas10/browser_dashboard.png)
